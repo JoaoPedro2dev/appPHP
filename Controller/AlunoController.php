9c0301cf29d1349aee28a20f27284d9a7a701956
JoaoPedro2dev <joaopdev1@gmail.com>
2025-04-14 15:16:19 -0300

    delete de alunos e inicio dos formularios

--- Controller/AlunoController.php
+++ Controller/AlunoController.php
@@ -54,7 +54,18 @@ final class AlunosController extends Controller
 
     public static function delete() : void
     {
+        parent::isProtected();
 
+        $model = new Aluno;
+
+        try
+        {
+            $model->delete((int) $_GET['id']);
+            parent::redirect("/aluno");
+        }catch(Exception $e){
+            $model -> setError("Ocorreu um error ao excluir o aluno:");
+            $model -> setError($e->getMessage());
+        }
     }
 }
 
